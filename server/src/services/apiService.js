3fbcc449532cb4bb5a79eaa8d53b16832c1c3529
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-11-12 20:56:50 +0000

    :sparkles: feat: add dynamic route handling for flexible API requests

--- server/src/services/apiService.js
+++ server/src/services/apiService.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const fetchDataApi =  async (params) => {
-  const url = `${process.env.API_URL}/${params}`
+const fetchDataApi =  async (dynamicPath, query) => {
+  const url = `${process.env.API_URL}/${dynamicPath}`
   try {
     const apiResponse = await axios.get(url, {
+      params: query,
       headers: {
         'x-rapidapi-key': process.env.API_KEY,
         'x-rapidapi-host': process.env.API_HOST,
