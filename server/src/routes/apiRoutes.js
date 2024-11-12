6d7f7283b28910aad946832057e520b58ec6ccda
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-11-12 19:55:58 +0000

    :sparkles: feat: create the main route to consume the api

--- server/src/routes/apiRoutes.js
+++ server/src/routes/apiRoutes.js
@@ -1,5 +1,8 @@
 import express from "express";
+import getApiData from "../controllers/apiController";
 
 const expressRounter = express.Router();
+
+expressRounter.get('/data', getApiData);
 
 export default expressRounter;
